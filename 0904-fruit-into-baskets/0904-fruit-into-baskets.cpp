a12dd99c49eb4821903ef24309654a0b6bd1875e
ranv73728 <ranv73728@gmail.com>
2026-08-19 11:54:57 +0530

    Time: 40 ms (68.89%), Space: 84.9 MB (34.46%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -1,36 +1,39 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        int n=fruits.size();
-        int l=0;
-        int r=0;
-        int count=0;
-        unordered_map<int,int> mp;
-        while(r<n){
+        int n = fruits.size();
+
+        int l = 0;
+        int r = 0;
+        int count = 0;
+
+        unordered_map<int, int> mp;
+
+        while (r < n) {
+
+            // Add current fruit
             mp[fruits[r]]++;
 
-            if(mp.size()<=2){
-                count=max(count,r-l+1);
-                
-            }
-            else{
+            // If there are more than 2 types,
+            // move l until there are at most 2 types
+            while (mp.size() > 2) {
+
                 mp[fruits[l]]--;
-                if(mp[fruits[l]]==0){
+
+                if (mp[fruits[l]] == 0) {
                     mp.erase(fruits[l]);
                 }
+
                 l++;
-               
             }
-            r=r+1;
-            
 
-                
-            
-            
-            
+            // Current window is valid
+            count = max(count, r - l + 1);
 
+            // Move right pointer
+            r++;
         }
+
         return count;
-        
     }
-}; 
+};
